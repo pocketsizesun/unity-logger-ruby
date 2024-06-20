99a3c95c321f0e04550dcac7f3563570ed2b0d67
Julien D. <julien@pocketsizesun.com>
2024-06-20 10:02:51 +0200

    Release v1.3.1
    faster JSON serialization

--- lib/unity/logger.rb
+++ lib/unity/logger.rb
@@ -9,7 +9,7 @@ module Unity
   class Logger < ::Logger
     attr_reader :source, :orig_logger
 
-    VERSION = '1.3.0'
+    VERSION = '1.3.1'
 
     DEBUG  = 0
     INFO   = 1
@@ -17,23 +17,28 @@ module Unity
     ERROR  = 3
     FATAL  = 4
 
+    JSON_FAST_GENERATE_OPTS = { mode: :null }.freeze
+    SEVERITY_KEY = '@severity'
+    DATE_KEY     = '@date'
+    HOSTNAME_KEY = '@hostname'
+    SOURCE_KEY   = '@source'
+    MESSAGE_KEY  = 'message'
+
     def initialize(*args, **kwargs)
       @source = kwargs.delete(:source)
       @local_hostname = kwargs.delete(:hostname) || Socket.gethostname
       super(*args, **kwargs)
 
       self.formatter = proc do |severity, datetime, progname, arg|
-        JSON.fast_generate(
-          {
-            '@severity' => severity,
-            '@date' => datetime.utc.iso8601,
-            '@hostname' => @local_hostname,
-            '@source' => @source
-          }.merge!(
-            arg.is_a?(Hash) ? arg : { 'message' => arg.to_s }
-          ),
-          mode: :null
-        ) + "\n"
+        data = {
+          SEVERITY_KEY => severity,
+          DATE_KEY     => datetime.utc.iso8601,
+          HOSTNAME_KEY => @local_hostname,
+          SOURCE_KEY   => @source
+        }.merge!(
+          arg.is_a?(Hash) ? arg : { MESSAGE_KEY => arg.to_s }
+        )
+        JSON.fast_generate(data, **JSON_FAST_GENERATE_OPTS) + "\n"
       end
     end
 
